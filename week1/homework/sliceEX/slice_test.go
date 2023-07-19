923c8fb65d6c946444f754d790823b708172ee1c
Gnoloayoul <631821745@qq.com>
2023-07-19 21:35:51 +0800

    20230719 完善测试代码

--- week1/homework/sliceEX/slice_test.go
+++ week1/homework/sliceEX/slice_test.go
@@ -1,4 +1,4 @@
-package sliceEX
+package sliceEX_test
 
 import (
 	"github.com/Gnoloayoul/JGEBCamp/week1/homework/sliceEX"
@@ -47,5 +47,60 @@ func TestSliceV02(t *testing.T) {
 		if !reflect.DeepEqual(result, test.expected) {
 			t.Errorf("Input: %v, %d\nExpected: %v\nGot: %v", test.inputSlice, test.inputN, test.expected, result)
 		}
+	}
+}
+
+func TestSliceV03(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		inputSlice []interface{}
+		inputN     int
+		expected   []interface{}
+	}{
+		{[]interface{}{1, 2, 3, 4, 5}, 2, []interface{}{1, 2, 4, 5}},
+		{[]interface{}{10, 20, 30, 40, 50}, 3, []interface{}{10, 20, 30, 50}},
+		{[]interface{}{100}, 0, []interface{}{}},
+		{[]interface{}{"a", "b", "c", "d", "e", "f", "g"}, 3, []interface{}{"a", "b", "c", "e", "f", "g"}},
+		{[]interface{}{}, 0, []interface{}{}},
+	}
+
+	for _, test := range tests {
+		result := sliceEX.SliceV03(test.inputSlice, test.inputN)
+		if !reflect.DeepEqual(result, test.expected) {
+			t.Errorf("Input: %v, %d\nExpected: %v\nGot: %v", test.inputSlice, test.inputN, test.expected, result)
+		}
+	}
+}
+
+func BenchmarkSliceV01(b *testing.B) {
+	slice := []int{1, 2, 3, 4, 5}
+	index := 2
+	for i := 0; i < b.N; i++ {
+		sliceEX.SliceV01(slice, index)
+	}
+}
+
+func BenchmarkSliceV02(b *testing.B) {
+	slice := []int{1, 2, 3, 4, 5}
+	index := 2
+	for i := 0; i < b.N; i++ {
+		sliceEX.SliceV02(slice, index)
+	}
+}
+
+func BenchmarkSliceV03I(b *testing.B) {
+	slice := []interface{}{1, 2, 3, 4, 5}
+	index := 2
+	for i := 0; i < b.N; i++ {
+		sliceEX.SliceV03(slice, index)
+	}
+}
+
+func BenchmarkSliceV03A(b *testing.B) {
+	slice := []interface{}{"a", "b", "c", "d", "e"}
+	index := 2
+	for i := 0; i < b.N; i++ {
+		sliceEX.SliceV03(slice, index)
 	}
 }
