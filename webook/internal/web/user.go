736dd73e0af01522400b48db1dea2c5db3980883
Gnoloayoul <631821745@qq.com>
2023-08-02 20:38:15 +0800

    20230802 直播前

--- webook/internal/web/user.go
+++ webook/internal/web/user.go
@@ -34,19 +34,26 @@ func (u *UserHandler) RegisterRoutes(server *gin.Engine) {
 		ug.POST("/edit", u.Edit)
 		ug.GET("/profile", u.Profile)
 		ug.POST("/delete", u.Delete)
+		//// 临时signup.HTML用的
+		//ug.GET("/index", u.Index)
 	}
 
 }
 
 func (u *UserHandler) SignUp(c *gin.Context) {
 	type SignUpReq struct {
+		// 临时signup.HTML用的
+		//Email string `form:"email"`
+		//ConfirmPassword string `form:"confirmPassword"`
+		//Password string `form:"password"`
+
 		Email string `json:"email"`
 		ConfirmPassword string `json:"confirmPassword"`
 		Password string `json:"password"`
 	}
 
 	var req SignUpReq
-	if err := c.Bind(&req); err != nil {
+	if err := c.ShouldBind(&req); err != nil {
 		return
 	}
 
@@ -76,9 +83,14 @@ func (u *UserHandler) SignUp(c *gin.Context) {
 	}
 
 	c.String(http.StatusOK, "注册成功")
-	fmt.Printf("%v", req)
+	fmt.Printf("%#v", req)
 	// 接下来是数据库操作
 }
+
+//// 进入临时signup.HTML用的
+//func (u *UserHandler) Index(c *gin.Context) {
+//	 c.HTML(http.StatusOK, "signup.html", nil)
+//}
 
 func (u *UserHandler) Edit(c *gin.Context) {
 
--- webook/main.go
+++ webook/main.go
@@ -3,17 +3,17 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/Gnoloayoul/JGEBCamp/webook/internal/web"
-	"net/http"
+
 )
 
 func main() {
 	server := gin.Default()
-	// 临时用的signup页面
+
 	u := web.NewUserHandler()
 	u.RegisterRoutes(server)
-	server.LoadHTMLFiles("../webook-fe/signup.html")
-	server.GET("/user/index", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "signup.html", nil)
-	})
+
+	//// 临时用的signup页面
+	//server.LoadHTMLFiles("../webook-fe/signup.html")
+
 	server.Run(":8080")
 }
