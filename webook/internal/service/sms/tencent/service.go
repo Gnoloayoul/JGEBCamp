478f5b016d988234c47be0712dabc328c012c324
Gnoloayoul <631821745@qq.com>
2023-08-25 21:29:19 +0800

    20230825 准备验证码测试

--- webook/internal/service/sms/tencent/service.go
+++ webook/internal/service/sms/tencent/service.go
@@ -9,9 +9,39 @@ import (
 )
 
 type Service struct {
-	addId *string
+	appId    *string
 	signName *string
-	client *sms.Client
+	client   *sms.Client
 }
 
-func NewService(client *sms.Client)
+func NewService(client *sms.Client, appId string, signName string) *Service {
+	return &Service{
+		client:   client,
+		appId:    ekit.ToPtr[string](appId),
+		signName: ekit.ToPtr[string](signName),
+	}
+}
+
+func (s *Service) Send(ctx context.Context, tplId string, args []string, numbers ...string) error {
+	req := sms.NewSendSmsRequest()
+	req.SmsSdkAppId = s.appId
+	req.TemplateId = ekit.ToPtr[string](tplId)
+	req.PhoneNumberSet = s.toStringPtrSlice(numbers)
+	req.TemplateParamSet = s.toStringPtrSlice(args)
+	resp, err := s.client.SendSms(req)
+	if err != nil {
+		return err
+	}
+	for _, status := range resp.Response.SendStatusSet {
+		if status.Code == nil || *(status.Code) != "ok" {
+			return fmt.Errorf("发送短信失败 %s, %s", *status.Code, *status.Message)
+		}
+	}
+	return nil
+}
+
+func (s *Service) toStringPtrSlice(src []string) []*string {
+	return slice.Map[string, *string](src, func(idx int, src string) *string {
+		return &src
+	})
+}
