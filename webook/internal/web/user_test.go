ed11d4f349c0c9e70ad30d685380d87e331f80e6
Gnoloayoul <631821745@qq.com>
2023-09-18 15:35:59 +0800

    handler_SignUp_test done -20230918
    Signed-off-by: Gnoloayoul <631821745@qq.com>

--- webook/internal/web/user_test.go
+++ webook/internal/web/user_test.go
@@ -74,13 +74,13 @@ func TestUserHandler_SignUp(t *testing.T) {
 
 		wantCode int
 		wantBody string
-	} {
+	}{
 		{
 			name: "注册成功",
 			mock: func(ctrl *gomock.Controller) service.UserService {
 				usersvc := svcmocks.NewMockUserService(ctrl)
 				usersvc.EXPECT().SignUp(gomock.Any(), domain.User{
-					Email: "631821745@qq.com",
+					Email:    "631821745@qq.com",
 					Password: "hello#world123",
 				}).Return(nil)
 				// 注册成功是 return nil
@@ -95,7 +95,6 @@ func TestUserHandler_SignUp(t *testing.T) {
 `,
 			wantCode: http.StatusOK,
 			wantBody: "注册成功",
-
 		},
 		{
 			name: "参数不对， bind 失败",
@@ -111,8 +110,6 @@ func TestUserHandler_SignUp(t *testing.T) {
 }
 `,
 			wantCode: http.StatusBadRequest,
-			wantBody: "注册成功",
-
 		},
 		{
 			name: "邮箱格式不对",
@@ -128,8 +125,7 @@ func TestUserHandler_SignUp(t *testing.T) {
 }
 `,
 			wantCode: http.StatusOK,
-			wantBody: "邮箱格式不对",
-
+			wantBody: "输入的邮箱格式不对",
 		},
 		{
 			name: "两次输入密码不匹配",
@@ -145,8 +141,7 @@ func TestUserHandler_SignUp(t *testing.T) {
 }
 `,
 			wantCode: http.StatusOK,
-			wantBody: "两次输入密码不匹配",
-
+			wantBody: "两次输入的密码不一致",
 		},
 		{
 			name: "密码格式不对",
@@ -164,15 +159,14 @@ func TestUserHandler_SignUp(t *testing.T) {
 }
 `,
 			wantCode: http.StatusOK,
-			wantBody: "密码格式不对",
-
+			wantBody: "密码必须大于8位，包含数字、特殊字符",
 		},
 		{
 			name: "邮箱冲突",
 			mock: func(ctrl *gomock.Controller) service.UserService {
 				usersvc := svcmocks.NewMockUserService(ctrl)
 				usersvc.EXPECT().SignUp(gomock.Any(), domain.User{
-					Email: "745@qq.com",
+					Email:    "745@qq.com",
 					Password: "hello#world123",
 				}).Return(service.ErrUserDuplicateEmail)
 				// 注册成功是 return nil
@@ -187,14 +181,13 @@ func TestUserHandler_SignUp(t *testing.T) {
 `,
 			wantCode: http.StatusOK,
 			wantBody: "邮箱冲突",
-
 		},
 		{
 			name: "系统异常",
 			mock: func(ctrl *gomock.Controller) service.UserService {
 				usersvc := svcmocks.NewMockUserService(ctrl)
 				usersvc.EXPECT().SignUp(gomock.Any(), domain.User{
-					Email: "631821745@qq.com",
+					Email:    "631821745@qq.com",
 					Password: "hello#world123",
 				}).Return(errors.New("随便一个error"))
 				// 注册成功是 return nil
@@ -203,18 +196,17 @@ func TestUserHandler_SignUp(t *testing.T) {
 			reqBody: `
 {
 	"email": "631821745@qq.com",
-	"password": "hello#world123", 
+	"password": "hello#world123",
 	"confirmPassword": "hello#world123"
 }
 `,
 			wantCode: http.StatusOK,
 			wantBody: "系统异常",
-
 		},
 	}
 
 	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T){
+		t.Run(tc.name, func(t *testing.T) {
 			ctrl := gomock.NewController(t)
 			defer ctrl.Finish()
 
@@ -226,7 +218,7 @@ func TestUserHandler_SignUp(t *testing.T) {
 
 			// 构建请求
 			// 用http.NewRequest
-			req, err := http.NewRequest(http.MethodPost, "users/signup", bytes.NewBuffer([]byte(tc.reqBody)))
+			req, err := http.NewRequest(http.MethodPost, "/users/signup", bytes.NewBuffer([]byte(tc.reqBody)))
 			// 这里假定url绝对不会错，不会引发err
 			require.NoError(t, err)
 			// 可以继续使用 req
