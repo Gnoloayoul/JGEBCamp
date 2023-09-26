9721e0215af6ec028ea260b6df1ea4960f35c921
Gnoloayoul <631821745@qq.com>
2023-09-26 18:00:42 +0800

    -20230926
    Signed-off-by: Gnoloayoul <631821745@qq.com>

---
+++ webook/internal/integration/user_test.go
@@ -0,0 +1,113 @@
+package integration
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"github.com/Gnoloayoul/JGEBCamp/webook/internal/web"
+	"github.com/Gnoloayoul/JGEBCamp/webook/ioc"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestUserHandler_e2e_SendLoginSMSCode(t *testing.T)  {
+	server := InitWebServer()
+	rdb := ioc.InitRedis()
+	testCases := []struct{
+		name string
+
+		// 提前准备数据
+		before func(t *testing.T)
+
+		// 验证并且删除数据
+		after func(t *testing.T)
+		reqBody string
+
+		wantCode int
+		wantBody web.Result
+	}{
+		{
+			name: "发送成功",
+			before: func(t *testing.T) {
+				// 不需要，也就是 Redis 什么数据也没有
+			},
+			after: func(t *testing.T) {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second * 4)
+				val, err := rdb.GetDel(ctx, "phone_code:login:123xxxxxxxx").Result()
+				cancel()
+				assert.NoError(t, err)
+				// 验证码是6位
+				assert.True(t, len(val) == 6)
+			},
+			reqBody:
+`
+{
+	"phone": "123xxxxxxxx",
+	
+}
+`,
+
+			wantCode: http.StatusOK,
+			wantBody: web.Result{
+				Code: 0,
+				Msg: "发送成功",
+			},
+		},
+		{
+			name: "发送太频繁",
+			before: func(t *testing.T) {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second * 4)
+				_, err := rdb.Set(ctx, "phone_code:login:123xxxxxxxx", "123456", time.Minute * 9 + time.Second * 30).Result()
+				cancel()
+				assert.NoError(t, err)
+			},
+			after: func(t *testing.T) {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second * 4)
+				val, err := rdb.GetDel(ctx, "phone_code:login:123xxxxxxxx").Result()
+				cancel()
+				assert.NoError(t, err)
+				// 验证码是6位
+				assert.True(t, len(val) == 6)
+			},
+			reqBody:
+			`
+{
+	"phone": "123xxxxxxxx",
+	
+}
+`,
+
+			wantCode: http.StatusOK,
+			wantBody: web.Result{
+				Code: 0,
+				Msg: "发送成功",
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T){
+			tc.before(t)
+			req, err := http.NewRequest(http.MethodPost, "/users/login_sms/code/send", bytes.NewBuffer([]byte(tc.reqBody)))
+			require.NoError(t, err)
+			req.Header.Set("Content-Type", "application/json")
+
+			resp := httptest.NewRecorder()
+			t.Log(resp)
+
+			server.ServeHTTP(resp, req)
+
+			var res web.Result
+			err = json.NewDecoder(resp.Body).Decode(&res)
+			require.NoError(t, err)
+
+			assert.Equal(t, tc.wantCode, resp.Code)
+			assert.Equal(t, tc.wantBody, res)
+			tc.after(t)
+		})
+	}
+}
---
+++ webook/internal/integration/wire.go
@@ -0,0 +1,36 @@
+//go:build wireinject
+
+package integration
+
+import (
+	"github.com/Gnoloayoul/JGEBCamp/webook/internal/repository"
+	"github.com/Gnoloayoul/JGEBCamp/webook/internal/repository/cache"
+	"github.com/Gnoloayoul/JGEBCamp/webook/internal/repository/dao"
+	"github.com/Gnoloayoul/JGEBCamp/webook/internal/service"
+	"github.com/Gnoloayoul/JGEBCamp/webook/internal/web"
+	"github.com/Gnoloayoul/JGEBCamp/webook/ioc"
+	"github.com/gin-gonic/gin"
+	"github.com/google/wire"
+)
+
+func InitWebServer() *gin.Engine {
+	wire.Build(
+		// 最底层的第三方依赖
+		ioc.InitDB, ioc.InitRedis,
+		// dao
+		dao.NewUserDAO,
+		// cache
+		cache.NewUserCache, cache.NewCodeRedisCache,
+		// repository
+		repository.NewUserRepository, repository.NewCodeRepository,
+		// service
+		service.NewUserService, service.NewCodeService,
+		// 基于内存实现
+		ioc.InitSMSService,
+
+		web.NewUserHandler,
+
+		ioc.InitGin, ioc.InitMiddlewares,
+	)
+	return new(gin.Engine)
+}
