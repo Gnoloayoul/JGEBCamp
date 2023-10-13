185394e5a276865f179edc44a355e293eb941140
Gnoloayoul <631821745@qq.com>
2023-10-13 12:23:45 +0800

    完成V1的单元测试 -20231013
    Signed-off-by: Gnoloayoul <631821745@qq.com>

--- webook/internal/service/sms/ratelimit/serviceV1_test.go
+++ webook/internal/service/sms/ratelimit/serviceV1_test.go
@@ -2,8 +2,12 @@ package ratelimit
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"github.com/Gnoloayoul/JGEBCamp/webook/internal/service/sms"
+	smsmocks "github.com/Gnoloayoul/JGEBCamp/webook/internal/service/sms/mocks"
 	"github.com/Gnoloayoul/JGEBCamp/webook/pkg/ratelimit"
+	ratelimitmocks "github.com/Gnoloayoul/JGEBCamp/webook/pkg/ratelimit/mocks"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/mock/gomock"
 	"testing"
@@ -16,7 +20,45 @@ func TestRatelimitSMSServiceV1_Send(t *testing.T) {
 
 		wantErr error
 	}{
-		{},
+		{
+			name: "正常发送",
+			mock: func(ctrl *gomock.Controller) (sms.Service, ratelimit.Limiter) {
+				sms := smsmocks.NewMockService(ctrl)
+				limit := ratelimitmocks.NewMockLimiter(ctrl)
+
+				limit.EXPECT().Limit(gomock.Any(), gomock.Any()).Return(false, nil)
+				sms.EXPECT().Send(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Return(nil)
+
+				return sms, limit
+			},
+
+			wantErr: nil,
+		},
+		{
+			name: "限流功能出问题",
+			mock: func(ctrl *gomock.Controller) (sms.Service, ratelimit.Limiter) {
+				sms := smsmocks.NewMockService(ctrl)
+				limit := ratelimitmocks.NewMockLimiter(ctrl)
+
+				limit.EXPECT().Limit(gomock.Any(), gomock.Any()).Return(false, errors.New("something wrong"))
+
+				return sms, limit
+			},
+
+			wantErr: fmt.Errorf("短信服务判断是否限流出现问题， %w", errors.New("something wrong")),
+		},
+		{
+			name: "触发限流",
+			mock: func(ctrl *gomock.Controller) (sms.Service, ratelimit.Limiter) {
+				sms := smsmocks.NewMockService(ctrl)
+				limit := ratelimitmocks.NewMockLimiter(ctrl)
+
+				limit.EXPECT().Limit(gomock.Any(), gomock.Any()).Return(true, nil)
+				return sms, limit
+			},
+
+			wantErr: errLimited,
+		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
@@ -26,7 +68,7 @@ func TestRatelimitSMSServiceV1_Send(t *testing.T) {
 			rss := NewRatelimitSMSServiceV1(tc.mock(ctrl))
 			err := rss.Send(context.Background(), "mytpl", []string{"123"}, "135xxxxxxxx")
 
-			assert.Equal(t, tc.WantErr, err)
+			assert.Equal(t, tc.wantErr, err)
 		})
 	}
 }
