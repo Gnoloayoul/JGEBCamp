1ae539a3ef1304f91a9c15f2c2ffa34f34ef3d1b
Gnoloayoul <631821745@qq.com>
2023-11-19 09:49:33 +0800

    opentelemetry -20231119
    Signed-off-by: Gnoloayoul <631821745@qq.com>

---
+++ opentelemetry/otel_test.go
@@ -0,0 +1,17 @@
+package opentelemetry
+
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/zipkin"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
+	"net/http"
+	"testing"
+	"time"
+)
