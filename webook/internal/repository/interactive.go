c650ce9d567361e81933de992b6cbc1a8b552c36
Gnoloayoul <631821745@qq.com>
2023-11-26 21:34:23 +0800

    repo/interactive -20231126
    Signed-off-by: Gnoloayoul <631821745@qq.com>

--- webook/internal/repository/interactive.go
+++ webook/internal/repository/interactive.go
@@ -1,6 +1,12 @@
 package repository
 
-import "github.com/Gnoloayoul/JGEBCamp/webook/internal/domain"
+import (
+	"context"
+	"github.com/Gnoloayoul/JGEBCamp/webook/internal/domain"
+	"github.com/Gnoloayoul/JGEBCamp/webook/internal/repository/cache"
+	"github.com/Gnoloayoul/JGEBCamp/webook/internal/repository/dao"
+	"github.com/Gnoloayoul/JGEBCamp/webook/pkg/logger"
+)
 
 //go:generate mockgen -source=./interactive.go -package=repomocks -destination=mocks/interactive.mock.go InteractiveRepository
 type InteractiveRepository interface {
@@ -22,3 +28,82 @@ type CachedReadCntRepository struct {
 	dao   dao.InteractiveDAO
 	l     logger.LoggerV1
 }
+
+func (c *CachedReadCntRepository) IncrReadCnt(ctx context.Context, biz string, bizId int64) error {
+	// 要考虑缓存方案了
+	// 这两个操作能不能换顺序？ —— 不能
+	err := c.dao.IncrReadCnt(ctx, biz, bizId)
+	if err != nil {
+		return err
+	}
+	//go func() {
+	//	c.cache.IncrReadCntIfPresent(ctx, biz, bizId)
+	//}()
+	//return err
+
+	return c.cache.IncrReadCntIfPresent(ctx, biz, bizId)
+}
+
+func (c *CachedReadCntRepository) BatchIncrReadCnt(ctx context.Context, biz []string, bizId []int64) error {
+	panic("implement me")
+}
+
+func (c *CachedReadCntRepository) IncrLike(ctx context.Context, biz string, bizId, uid int64) error {
+	// 先插入点赞，然后更新点赞计数，更新缓存
+	err := c.dao.InsertLikeInfo(ctx, biz, bizId, uid)
+	if err != nil {
+		return err
+	}
+	// 这种做法，你需要在 repository 层面上维持住事务
+	//c.dao.IncrLikeCnt()
+	return c.cache.IncrLikeCntIfPresent(ctx, biz, bizId)
+}
+
+func (c *CachedReadCntRepository) DecrLike(ctx context.Context, biz string, bizId, uid int64) error {
+	err := c.dao.DeleteLikeInfo(ctx, biz, bizId, uid)
+	if err != nil {
+		return err
+	}
+	return c.cache.DecrLikeCntIfPresent(ctx, biz, bizId)
+}
+
+func (c *CachedReadCntRepository) AddCollectionItem(ctx context.Context, biz string, bizId, cid int64, uid int64) error {
+	panic("implement me")
+}
+
+func (c *CachedReadCntRepository) Get(ctx context.Context, biz string, bizId int64) (domain.Interactive, error) {
+	panic("implement me")
+}
+
+func (c *CachedReadCntRepository) Liked(ctx context.Context, biz string, id int64, uid int64) (bool, error) {
+	_, err := c.dao.GetLikeInfo(ctx, biz, id, uid)
+	switch err {
+	case nil:
+		return true, nil
+	case dao.ErrRecordNotFound:
+		// 你要吞掉
+		return false, nil
+	default:
+		return false, err
+	}
+}
+
+func (c *CachedReadCntRepository) Collected(ctx context.Context, biz string, id int64, uid int64) (bool, error) {
+	_, err := c.dao.GetCollectionInfo(ctx, biz, id, uid)
+	switch err {
+	case nil:
+		return true, nil
+	case dao.ErrRecordNotFound:
+		// 你要吞掉
+		return false, nil
+	default:
+		return false, err
+	}
+}
+
+func (c *CachedReadCntRepository) AddRecord(ctx context.Context, aid int64, uid int64) error {
+	panic("implement me")
+}
+
+
+
