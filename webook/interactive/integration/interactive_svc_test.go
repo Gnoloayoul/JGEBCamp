f0a53d1a9e63050d64354f7d2cd2f6d612c2f6f8
Gnoloayoul <631821745@qq.com>
2024-02-22 16:14:42 +0800

    模块化ing 补测试2 -20240222
    Signed-off-by: Gnoloayoul <631821745@qq.com>

--- webook/interactive/integration/interactive_svc_test.go
+++ webook/interactive/integration/interactive_svc_test.go
@@ -2,11 +2,14 @@ package integration
 
 import (
 	"context"
+	"github.com/Gnoloayoul/JGEBCamp/webook/interactive/domain"
 	"github.com/Gnoloayoul/JGEBCamp/webook/interactive/integration/startup"
+	"github.com/Gnoloayoul/JGEBCamp/webook/interactive/repository/dao"
 	"github.com/redis/go-redis/v9"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 	"gorm.io/gorm"
+	"testing"
 	"time"
 )
 
@@ -37,4 +40,827 @@ func (s *InteractiveTestSuite) TearDownTest() {
 	// clear Redis
 	err = s.rdb.FlushDB(ctx).Err()
 	assert.NoError(s.T(), err)
+}
+
+func (s *InteractiveTestSuite) TestIncrReadCnt() {
+	t := s.T()
+	testCases := []struct {
+		name string
+		before func(t *testing.T)
+		after func(t *testing.T)
+
+		biz string
+		bizId int64
+
+		wantErr error
+	}{
+		{
+			name: "增加成功， db 和 redis ",
+			before: func(t *testing.T) {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second * 3)
+				defer cancel()
+
+				// 写入 db
+				err := s.db.Create(dao.Interactive{
+					Id: 1,
+					Biz: "test",
+					BizId: 2,
+					ReadCnt: 3,
+					CollectCnt: 4,
+					LikeCnt: 5,
+					Ctime: 6,
+					Utime: 7,
+				}).Error
+				assert.NoError(t, err)
+
+				// 写入 redis
+				err = s.rdb.HSet(ctx, "interactive:test:2",
+					"read_cnt", 3).Err()
+				assert.NoError(t, err)
+			},
+			after: func(t *testing.T) {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second * 3)
+				defer cancel()
+
+				// 测试 db 部分
+				var data dao.Interactive
+				err := s.db.Where("id = ?", 1).First(&data).Error
+				assert.NoError(t, err)
+				assert.True(t, data.Utime > 7)
+				data.Utime = 0
+				assert.Equal(t, dao.Interactive{
+					Id: 1,
+					Biz: "test",
+					BizId: 2,
+					// （查询）阅读 + 1
+					ReadCnt: 4,
+					CollectCnt: 4,
+					LikeCnt: 5,
+					Ctime: 6,
+				}, data)
+
+				// 测试 redis 部分 + 清除数据
+				cnt, err := s.rdb.HGet(ctx, "interactive:test:2", "read_cnt").Int()
+				assert.NoError(t, err)
+				assert.Equal(t, 4, cnt)
+				err = s.rdb.Del(ctx, "interactive:test:2").Err()
+				assert.NoError(t, err)
+			},
+			biz: "test",
+			bizId: 2,
+		},
+		{
+			name: "增加成功， db 有， redis（缓存）没有",
+			before: func(t *testing.T) {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second * 3)
+				defer cancel()
+
+				// 写入 db
+				err := s.db.WithContext(ctx).Create(dao.Interactive{
+					Id: 3,
+					Biz: "test",
+					BizId: 3,
+					ReadCnt: 3,
+					CollectCnt: 4,
+					LikeCnt: 5,
+					Ctime: 6,
+					Utime: 7,
+				}).Error
+				assert.NoError(t, err)
+			},
+			after: func(t *testing.T) {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second * 3)
+				defer cancel()
+
+				// 测试 db 部分
+				var data dao.Interactive
+				err := s.db.Where("id = ?", 3).First(&data).Error
+				assert.NoError(t, err)
+				assert.True(t, data.Utime > 7)
+				data.Utime = 0
+				assert.Equal(t, dao.Interactive{
+					Id: 3,
+					Biz: "test",
+					BizId: 3,
+					// （查询）阅读 + 1
+					ReadCnt: 4,
+					CollectCnt: 4,
+					LikeCnt: 5,
+					Ctime: 6,
+				}, data)
+
+				// 测试 redis 部分 + 清除数据
+				cnt, err := s.rdb.Exists(ctx, "interactive:test:3").Result()
+				assert.NoError(t, err)
+				assert.Equal(t, int64(0), cnt)
+			},
+			biz: "test",
+			bizId: 3,
+		},
+		{
+			name: "增加成功，都没有",
+			before: func(t *testing.T) {},
+			after: func(t *testing.T) {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second * 3)
+				defer cancel()
+
+				// 测试 db 部分
+				var data dao.Interactive
+				err := s.db.Where("biz = ? AND biz_id = ?", "test",3).First(&data).Error
+				assert.NoError(t, err)
+				assert.True(t, data.Utime > 0)
+				assert.True(t, data.Ctime > 0)
+				assert.True(t, data.Id > 0)
+				data.Id = 0
+				data.Utime = 0
+				data.Ctime = 0
+				assert.Equal(t, dao.Interactive{
+					Biz: "test",
+					BizId: 4,
+					ReadCnt: 1,
+				}, data)
+
+				// 测试 redis 部分 + 清除数据
+				cnt, err := s.rdb.Exists(ctx, "interactive:test:4").Result()
+				assert.NoError(t, err)
+				assert.Equal(t, int64(0), cnt)
+			},
+			biz: "test",
+			bizId: 4,
+		},
+	}
+
+	// 启动服务，准备测试
+	svc := startup.InitInteractiveService()
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// 初始化测试
+			tc.before(t)
+
+			// 运行测试
+			err := svc.IncrReadCnt(context.Background(), tc.biz, tc.bizId)
+			assert.Equal(t, tc.wantErr, err)
+
+			// 测试后归零
+			tc.after(t)
+		})
+	}
+}
+
+func (s *InteractiveTestSuite) TestLike() {
+	t := s.T()
+	testCases := []struct {
+		name string
+		before func(t *testing.T)
+		after func(t *testing.T)
+
+		biz string
+		bizId int64
+		uid int64
+
+		wantErr error
+	}{
+		{
+			name: "点赞成功， db 和 redis 都有",
+			before: func(t *testing.T) {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second * 3)
+				defer cancel()
+
+				// 写入 db
+				err := s.db.Create(dao.Interactive{
+					Id: 1,
+					Biz: "test",
+					BizId: 2,
+					ReadCnt: 3,
+					CollectCnt: 4,
+					LikeCnt: 5,
+					Ctime: 6,
+					Utime: 7,
+				}).Error
+				assert.NoError(t, err)
+
+				// 写入 redis
+				err = s.rdb.HSet(ctx, "interactive:test:2",
+					"like_cnt", 3).Err()
+				assert.NoError(t, err)
+			},
+			after: func(t *testing.T) {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second * 3)
+				defer cancel()
+
+				// 测试 db 部分
+				var data dao.Interactive
+				err := s.db.Where("id = ?", 1).First(&data).Error
+				assert.NoError(t, err)
+				assert.True(t, data.Utime > 7)
+				data.Utime = 0
+				assert.Equal(t, dao.Interactive{
+					Id: 1,
+					Biz: "test",
+					BizId: 2,
+					ReadCnt: 3,
+					CollectCnt: 4,
+					// 点赞 + 1
+					LikeCnt: 6,
+					Ctime: 6,
+				}, data)
+
+				var likeBiz dao.UserLikeBiz
+				err = s.db.Where("biz = ? AND biz_id = ? AND uid = ?",
+					"test", 2, 123).First(&likeBiz).Error
+				assert.NoError(t, err)
+				assert.True(t, likeBiz.Id > 0)
+				assert.True(t, likeBiz.Ctime > 0)
+				assert.True(t, likeBiz.Utime > 0)
+				likeBiz.Id = 0
+				likeBiz.Ctime = 0
+				likeBiz.Utime = 0
+				assert.Equal(t, dao.UserLikeBiz{
+					Biz: "test",
+					BizId: 2,
+					Uid: 123,
+					Status: 1,
+				}, likeBiz)
+
+				// 测试 redis 部分 + 清除数据
+				cnt, err := s.rdb.HGet(ctx, "interactive:test:2", "like_cnt").Int()
+				assert.NoError(t, err)
+				assert.Equal(t, 4, cnt)
+				err = s.rdb.Del(ctx, "interactive:test:2").Err()
+				assert.NoError(t, err)
+			},
+			biz: "test",
+			bizId: 2,
+			uid: 123,
+		},
+		{
+			name: "点赞成功， db 和 redis 都没有",
+			before: func(t *testing.T) {},
+			after: func(t *testing.T) {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second * 3)
+				defer cancel()
+
+				// 测试 db 部分
+				var data dao.Interactive
+				err := s.db.Where("biz = ? AND biz_id = ?", "test", 3).First(&data).Error
+				assert.NoError(t, err)
+				assert.True(t, data.Id > 0)
+				assert.True(t, data.Ctime > 0)
+				assert.True(t, data.Utime > 0)
+				data.Id = 0
+				data.Ctime = 0
+				data.Utime = 0
+				assert.Equal(t, dao.Interactive{
+					Biz: "test",
+					BizId: 3,
+					LikeCnt: 1,
+				}, data)
+
+				var likeBiz dao.UserLikeBiz
+				err = s.db.Where("biz = ? AND biz_id = ? AND uid = ?",
+					"test", 3, 123).First(&likeBiz).Error
+				assert.NoError(t, err)
+				assert.True(t, likeBiz.Id > 0)
+				assert.True(t, likeBiz.Ctime > 0)
+				assert.True(t, likeBiz.Utime > 0)
+				likeBiz.Id = 0
+				likeBiz.Ctime = 0
+				likeBiz.Utime = 0
+				assert.Equal(t, dao.UserLikeBiz{
+					Biz: "test",
+					BizId: 3,
+					Uid: 123,
+					Status: 1,
+				}, likeBiz)
+
+				// 测试 redis 部分 + 清除数据
+				cnt, err := s.rdb.HGet(ctx, "interactive:test:2", "like_cnt").Result()
+				assert.NoError(t, err)
+				assert.Equal(t, int64(0), cnt)
+			},
+			biz: "test",
+			bizId: 3,
+			uid: 123,
+		},
+	}
+
+	// 启动服务，准备测试
+	svc := startup.InitInteractiveService()
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// 初始化测试
+			tc.before(t)
+
+			// 运行测试
+			err := svc.Like(context.Background(), tc.biz, tc.bizId, tc.uid)
+			assert.Equal(t, tc.wantErr, err)
+
+			// 测试后归零
+			tc.after(t)
+		})
+	}
+}
+
+func (s *InteractiveTestSuite) TestDisLike() {
+	t := s.T()
+	testCases := []struct {
+		name string
+		before func(t *testing.T)
+		after func(t *testing.T)
+
+		biz string
+		bizId int64
+		uid int64
+
+		wantErr error
+	}{
+		{
+			name: "取消点赞成功， db 和 redis 都有",
+			before: func(t *testing.T) {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second * 3)
+				defer cancel()
+
+				// 写入 db
+				err := s.db.Create(dao.Interactive{
+					Id: 1,
+					Biz: "test",
+					BizId: 2,
+					ReadCnt: 3,
+					CollectCnt: 4,
+					LikeCnt: 5,
+					Ctime: 6,
+					Utime: 7,
+				}).Error
+				assert.NoError(t, err)
+
+				err = s.db.Create(dao.UserLikeBiz{
+					Id: 1,
+					Biz: "test",
+					BizId: 2,
+					Uid: 123,
+					Ctime: 6,
+					Utime: 7,
+					Status: 1,
+				}).Error
+				assert.NoError(t, err)
+
+				// 写入 redis
+				err = s.rdb.HSet(ctx, "interactive:test:2",
+					"like_cnt", 3).Err()
+				assert.NoError(t, err)
+			},
+			after: func(t *testing.T) {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second * 3)
+				defer cancel()
+
+				// 测试 db 部分
+				var data dao.Interactive
+				err := s.db.Where("id = ?", 1).First(&data).Error
+				assert.NoError(t, err)
+				assert.True(t, data.Utime > 7)
+				data.Utime = 0
+				assert.Equal(t, dao.Interactive{
+					Id: 1,
+					Biz: "test",
+					BizId: 2,
+					ReadCnt: 3,
+					CollectCnt: 4,
+					// 点赞 - 1
+					LikeCnt: 5,
+					Ctime: 6,
+				}, data)
+
+				var likeBiz dao.UserLikeBiz
+				err = s.db.Where("id = ?", 1).First(&likeBiz).Error
+				assert.NoError(t, err)
+				assert.True(t, likeBiz.Utime > 0)
+				likeBiz.Utime = 0
+				assert.Equal(t, dao.UserLikeBiz{
+					Id: 1,
+					Biz: "test",
+					BizId: 2,
+					Uid: 123,
+					Ctime: 6,
+					Status: 0,
+				}, likeBiz)
+
+				// 测试 redis 部分 + 清除数据
+				cnt, err := s.rdb.HGet(ctx, "interactive:test:2", "like_cnt").Int()
+				assert.NoError(t, err)
+				assert.Equal(t, 2, cnt)
+				err = s.rdb.Del(ctx, "interactive:test:2").Err()
+				assert.NoError(t, err)
+			},
+			biz: "test",
+			bizId: 2,
+			uid: 123,
+		},
+
+	}
+
+	// 启动服务，准备测试
+	svc := startup.InitInteractiveService()
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// 初始化测试
+			tc.before(t)
+
+			// 运行测试
+			err := svc.CancelLike(context.Background(), tc.biz, tc.bizId, tc.uid)
+			assert.Equal(t, tc.wantErr, err)
+
+			// 测试后归零
+			tc.after(t)
+		})
+	}
+}
+
+func (s *InteractiveTestSuite) TestCollect() {
+	t := s.T()
+	testCases := []struct {
+		name string
+		before func(t *testing.T)
+		after func(t *testing.T)
+
+		biz string
+		bizId int64
+		cid int64
+		uid int64
+
+		wantErr error
+	}{
+		{
+			name: "收藏成功， db 和 redis ",
+			before: func(t *testing.T) {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second * 3)
+				defer cancel()
+
+				// 写入 db
+				err := s.db.WithContext(ctx).Create(&dao.Interactive{
+					Biz: "test",
+					BizId: 3,
+					CollectCnt: 10,
+					Ctime: 123,
+					Utime: 234,
+				}).Error
+				assert.NoError(t, err)
+
+				// 写入 redis
+				err = s.rdb.HSet(ctx, "interactive:test:3",
+					"collect_cnt", 10).Err()
+				assert.NoError(t, err)
+			},
+			after: func(t *testing.T) {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second * 3)
+				defer cancel()
+
+				// 测试 db 部分
+				var data dao.Interactive
+				err := s.db.WithContext(ctx).
+					Where("biz = ? AND biz_id = ?", "test", 3).First(&data).Error
+				assert.NoError(t, err)
+				assert.True(t, data.Id > 0)
+				assert.True(t, data.Ctime > 0)
+				assert.True(t, data.Utime > 0)
+				data.Id = 0
+				data.Ctime = 0
+				data.Utime = 0
+				assert.Equal(t, dao.Interactive{
+					Biz: "test",
+					BizId: 3,
+					CollectCnt: 11,
+				}, data)
+
+				var cdata dao.UserCollectionBiz
+				err = s.db.WithContext(ctx).
+					Where("uid = ? AND biz = ? AND biz_id = ?", 1, "test", 3).
+					First(&cdata).Error
+				assert.NoError(t, err)
+				assert.True(t, cdata.Id > 0)
+				assert.True(t, cdata.Ctime > 0)
+				assert.True(t, cdata.Utime > 0)
+				cdata.Id = 0
+				cdata.Ctime = 0
+				cdata.Utime = 0
+				assert.Equal(t, dao.UserCollectionBiz{
+					Biz: "test",
+					BizId: 3,
+					Cid: 1,
+					Uid: 1,
+				}, cdata)
+
+				// 测试 redis 部分 + 清除数据
+				cnt, err := s.rdb.HGet(ctx, "interactive:test:3", "collect_cnt").Int()
+				assert.NoError(t, err)
+				assert.Equal(t, 11, cnt)
+			},
+			biz: "test",
+			bizId: 3,
+			cid: 1,
+			uid: 1,
+		},
+		{
+			name: "收藏成功， db 有 redis(缓存) 没有",
+			before: func(t *testing.T) {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second * 3)
+				defer cancel()
+
+				// 写入 db
+				err := s.db.WithContext(ctx).Create(&dao.Interactive{
+					Biz: "test",
+					BizId: 2,
+					CollectCnt: 10,
+					Ctime: 123,
+					Utime: 234,
+				}).Error
+				assert.NoError(t, err)
+
+			},
+			after: func(t *testing.T) {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second * 3)
+				defer cancel()
+
+				// 测试 db 部分
+				var data dao.Interactive
+				err := s.db.WithContext(ctx).
+					Where("biz = ? AND biz_id = ?", "test", 2).First(&data).Error
+				assert.NoError(t, err)
+				assert.True(t, data.Id > 0)
+				assert.True(t, data.Ctime > 0)
+				assert.True(t, data.Utime > 0)
+				data.Id = 0
+				data.Ctime = 0
+				data.Utime = 0
+				assert.Equal(t, dao.Interactive{
+					Biz: "test",
+					BizId: 2,
+					CollectCnt: 11,
+				}, data)
+
+				var cdata dao.UserCollectionBiz
+				err = s.db.WithContext(ctx).
+					Where("uid = ? AND biz = ? AND biz_id = ?", 1, "test", 2).
+					First(&cdata).Error
+				assert.NoError(t, err)
+				assert.True(t, cdata.Id > 0)
+				assert.True(t, cdata.Ctime > 0)
+				assert.True(t, cdata.Utime > 0)
+				cdata.Id = 0
+				cdata.Ctime = 0
+				cdata.Utime = 0
+				assert.Equal(t, dao.UserCollectionBiz{
+					Biz: "test",
+					BizId: 2,
+					Cid: 1,
+					Uid: 1,
+				}, cdata)
+			},
+			biz: "test",
+			bizId: 2,
+			cid: 1,
+			uid: 1,
+		},
+		{
+			name: "收藏成功， db 和 redis(缓存) 都没有",
+			before: func(t *testing.T) {},
+			after: func(t *testing.T) {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second * 3)
+				defer cancel()
+
+				// 测试 db 部分
+				var data dao.Interactive
+				err := s.db.WithContext(ctx).
+					Where("biz = ? AND biz_id = ?", "test", 1).First(&data).Error
+				assert.NoError(t, err)
+				assert.True(t, data.Id > 0)
+				assert.True(t, data.Ctime > 0)
+				assert.True(t, data.Utime > 0)
+				data.Id = 0
+				data.Ctime = 0
+				data.Utime = 0
+				assert.Equal(t, dao.Interactive{
+					Biz: "test",
+					BizId: 1,
+					CollectCnt: 1,
+				}, data)
+
+				var cdata dao.UserCollectionBiz
+				err = s.db.WithContext(ctx).
+					Where("uid = ? AND biz = ? AND biz_id = ?", 1, "test", 1).
+					First(&cdata).Error
+				assert.NoError(t, err)
+				assert.True(t, cdata.Id > 0)
+				assert.True(t, cdata.Ctime > 0)
+				assert.True(t, cdata.Utime > 0)
+				cdata.Id = 0
+				cdata.Ctime = 0
+				cdata.Utime = 0
+				assert.Equal(t, dao.UserCollectionBiz{
+					Biz: "test",
+					BizId: 1,
+					Cid: 1,
+					Uid: 1,
+				}, cdata)
+
+				cnt, err := s.rdb.Exists(ctx, "interactive:test:1").Result()
+				assert.NoError(t, err)
+				assert.Equal(t, int64(0), cnt)
+			},
+			biz: "test",
+			bizId: 2,
+			cid: 1,
+			uid: 1,
+		},
+	}
+
+	// 启动服务，准备测试
+	svc := startup.InitInteractiveService()
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// 初始化测试
+			tc.before(t)
+
+			// 运行测试
+			err := svc.Collect(context.Background(), tc.biz, tc.bizId, tc.cid, tc.uid)
+			assert.Equal(t, tc.wantErr, err)
+
+			// 测试后归零
+			tc.after(t)
+		})
+	}
+}
+
+func (s *InteractiveTestSuite) TestGet() {
+	t := s.T()
+	testCases := []struct {
+		name string
+		before func(t *testing.T)
+
+		biz string
+		bizId int64
+		uid int64
+
+		wantErr error
+		wantRes domain.Interactive
+	}{
+		{
+			name: "全取出来了，没缓存",
+			before: func(t *testing.T) {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second * 3)
+				defer cancel()
+
+				// 写入 db
+				err := s.db.WithContext(ctx).Create(&dao.Interactive{
+					Biz: "test",
+					BizId: 12,
+					ReadCnt: 100,
+					CollectCnt: 200,
+					LikeCnt: 300,
+					Ctime: 123,
+					Utime: 234,
+				}).Error
+				assert.NoError(t, err)
+			},
+			biz: "test",
+			bizId: 12,
+			uid: 123,
+			wantRes: domain.Interactive{
+				BizId: 12,
+				ReadCnt: 100,
+				CollectCnt: 200,
+				LikeCnt: 300,
+			},
+		},
+		{
+			name: "全取出来了，命中缓存， 用户已经点赞收藏",
+			before: func(t *testing.T) {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second * 3)
+				defer cancel()
+
+				// 写入 db
+				err := s.db.WithContext(ctx).Create(&dao.UserLikeBiz{
+					Biz: "test",
+					BizId: 3,
+					Uid: 123,
+					Ctime: 123,
+					Utime: 234,
+					Status: 1,
+				}).Error
+				assert.NoError(t, err)
+
+				err = s.db.WithContext(ctx).Create(&dao.UserCollectionBiz{
+					Cid: 1,
+					Biz: "test",
+					BizId: 3,
+					Uid: 123,
+					Ctime: 123,
+					Utime: 234,
+				}).Error
+				assert.NoError(t, err)
+
+				err = s.rdb.HSet(ctx, "interactive:test:3",
+					"read_cnt", 0, "collect_cnt", 1).Err()
+				assert.NoError(t, err)
+			},
+			biz: "test",
+			bizId: 3,
+			uid: 123,
+			wantRes: domain.Interactive{
+				BizId: 3,
+				CollectCnt: 1,
+				Collected: true,
+				Liked: true,
+			},
+		},
+	}
+
+	// 启动服务，准备测试
+	svc := startup.InitInteractiveService()
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// 初始化测试
+			tc.before(t)
+
+			// 运行测试
+			res, err := svc.Get(context.Background(), tc.biz, tc.bizId, tc.uid)
+			assert.Equal(t, tc.wantErr, err)
+			assert.Equal(t, tc.wantRes, res)
+
+		})
+	}
+}
+
+func (s *InteractiveTestSuite) TestGetByIds() {
+	t := s.T()
+	perCtx, cancel := context.WithTimeout(context.Background(), time.Second * 3)
+	defer cancel()
+
+	// 准备数据
+	for i := 1; i < 5; i++ {
+		i := int64(i)
+		err := s.db.WithContext(perCtx).
+			Create(&dao.Interactive{
+				Id: i,
+				Biz: "test",
+				BizId: i,
+				ReadCnt: i,
+				CollectCnt: i + 1,
+				LikeCnt: i + 2,
+		}).Error
+		assert.NoError(t, err)
+	}
+
+
+	testCases := []struct {
+		name string
+		before func(t *testing.T)
+
+		biz string
+		ids []int64
+
+		wantErr error
+		wantRes map[int64]domain.Interactive
+	}{
+		{
+			name: "查找成功",
+			biz: "test",
+			ids: []int64{1, 2},
+			wantRes: map[int64]domain.Interactive{
+				1: {
+					BizId: 1,
+					ReadCnt: 1,
+					CollectCnt: 2,
+					LikeCnt: 3,
+				},
+				2: {
+					BizId: 2,
+					ReadCnt: 2,
+					CollectCnt: 3,
+					LikeCnt: 4,
+				},
+			},
+		},
+		{
+			name: "没有对应数据",
+			biz: "test",
+			ids: []int64{100, 200},
+			wantRes: map[int64]domain.Interactive{},
+		},
+	}
+
+	// 启动服务，准备测试
+	svc := startup.InitInteractiveService()
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+
+			// 运行测试
+			res, err := svc.GetByIds(context.Background(), tc.biz, tc.ids)
+			assert.Equal(t, tc.wantErr, err)
+			assert.Equal(t, tc.wantRes, res)
+		})
+	}
+}
+
+func TestInteractiveService(t *testing.T) {
+	suite.Run(t, &InteractiveTestSuite{})
 }
